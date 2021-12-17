078064b518ad12722db2cb9804a6c6d77f2c4779
jonghyeok34 <jonghyeok34@gmail.com>
2021-12-17 22:45:42 +0900

    add default spring boot

---
+++ src/main/java/com/company/app/AppApplication.java
@@ -0,0 +1,17 @@
+package com.company.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableAspectJAutoProxy
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
